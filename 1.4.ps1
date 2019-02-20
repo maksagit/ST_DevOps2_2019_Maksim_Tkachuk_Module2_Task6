c73f13658e11880f9243e1276ddca1927c09a773
Maksim Tkachuk <maksibon@gmail.com>
2019-02-20 17:00:02 +0300

    Task6 v0.951

--- 1.4.ps1
+++ 1.4.ps1
@@ -1,6 +1,6 @@
 ### 1.4. Расшарить папку на компьютере
 
 (Get-WmiObject -List -ComputerName . | `
-Where-Object -FilterScript {$_.Name –eq "Win32_Share"}).InvokeMethod("Create",("D:\git","GitShare",0,13,"My Git share!"))
+    Where-Object -FilterScript {$_.Name –eq "Win32_Share"}).InvokeMethod("Create",("D:\git","GitShare",0,13,"My Git share!"))
 
 # net share GitShare=D:\git /users:13 /remark:"My Git share!"
--- All.ps1
+++ All.ps1
@@ -56,7 +56,7 @@ Invoke-Command -ComputerName $hosts -Credential "S2012R2\Administrator" -ScriptB
 ### 1.4. Расшарить папку на компьютере
 
 (Get-WmiObject -List -ComputerName . | `
-Where-Object -FilterScript {$_.Name –eq "Win32_Share"}).InvokeMethod("Create",("D:\git","GitShare",0,13,"My Git share!"))
+    Where-Object -FilterScript {$_.Name –eq "Win32_Share"}).InvokeMethod("Create",("D:\git","GitShare",0,13,"My Git share!"))
 
 # net share GitShare=D:\git /users:13 /remark:"My Git share!"
 
